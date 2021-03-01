d1cc8f1c4f0cbfd667a2225633bed410f1dcb339
sinem atak <ataksinem89@gmail.com>
2021-03-01 05:22:27 -0500

    after github

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log('from Master Branch')
-console.log('from Master Branch > from add-js')
+console.log('from Master Branch');
+console.log('from Master Branch > from add-js');
+console.log('from Master Branch > from add-js=> After GitHub!!!');
